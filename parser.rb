87e5c82064e2a76a55669e918a605c5f3dbce379
Leo Ahnn <leoahnn@gmail.com>
2016-07-15 17:05:51 -0400

    working on html parser

--- parser.rb
+++ parser.rb
@@ -4,7 +4,7 @@ def load_html
   str = File.readlines('test.html').map(&:chomp).join
 end
 
-p load_html
+# p load_html
 def parser_script
 
   html = load_html
@@ -13,31 +13,15 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def parse_tag(tag)
   tags = {}
   usable = /<(.*)>/.match(tag).captures.join
   usable.gsub(/ =/,"=")
 
-  tags["type"] = usable[/^([\w\-]+)/]
+  tags["type"] = usable[/^([\w]+)/]
 
   quote_regex = /'(.*?)'/
-  equal_regex = /([\w\-]+)=/
+  equal_regex = /([\w]+)=/
 
   key = usable.scan(equal_regex).flatten
   info = usable.scan(quote_regex).flatten
@@ -49,8 +33,9 @@ def parse_tag(tag)
       tags[k] = info[index]
     end
   end
+  tags
 end
 
 
 # converts HTML into a hash containing key attributes
-parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
+p parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
