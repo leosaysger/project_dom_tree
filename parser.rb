89395228e44be26ea00091fcad76fca0998475a6
Alex Lach <alexglach@gmail.com>
2016-07-15 14:25:32 -0400

    starting on regex

--- parser.rb
+++ parser.rb
@@ -1,4 +1,22 @@
-class Parser
+
+
+def parse_tag(tag)
+  tags = {} 
+  usable = /<(.*)>/.match(tag).captures.join
+  puts usable
+  tags["type"] = usable[/^([\w\-]+)/]
+
+  to_find = /=/
+  
+  
+  tags
+
 
 end
+
+
 # converts HTML into a hash containing key attributes
+
+
+
+puts parse_tag("<p class='foo bar' id='baz' name='fozzie'>")
